e5a1fc4ceb8b4d886e0896daae3cd1ae9ad37ffc
Eslam Ashour <anaeslam20015@gmail.com>
2026-08-20 15:47:28 +0300

    Add File Pipes

--- src/app/topics/Components/Pipes/pipes/pipes/pipes.component.ts
+++ src/app/topics/Components/Pipes/pipes/pipes/pipes.component.ts
@@ -1,12 +1,37 @@
+import {
+  CurrencyPipe,
+  DatePipe,
+  JsonPipe,
+  LowerCasePipe,
+  PercentPipe,
+  SlicePipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe,
+    SlicePipe,
+    JsonPipe,
+    PercentPipe,
+    CurrencyPipe,
+  ],
   templateUrl: './pipes.component.html',
-  styleUrl: './pipes.component.css'
+  styleUrl: './pipes.component.css',
 })
 export class PipesComponent {
-
+  UserName = 'Ahmed';
+  UserData = new Date();
+  AllUserName = 'Ahmed Karem';
+  DataObject = {
+    name: 'Ahmed Ali',
+    age: 28,
+  };
+  discount = 0.7;
+  salary = 5000;
 }
